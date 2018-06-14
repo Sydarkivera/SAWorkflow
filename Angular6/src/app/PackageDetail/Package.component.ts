cf5e33b250eaebe6b84f0f63bd2e980f36f55c81
Sydarkivera <simon.nilsson@sydarkivera.se>
2018-06-14 07:19:55 +0200

    Added graphs

--- Angular6/src/app/PackageDetail/Package.component.ts
+++ Angular6/src/app/PackageDetail/Package.component.ts
@@ -15,6 +15,7 @@ export class PackageComponent {
   id: number;
   package: any;
   modules: [any];
+  placeholderLocation: number = -1;
 
   constructor(private packageService: PackageDetailService, private route: ActivatedRoute) {
     this.package = {
@@ -42,4 +43,93 @@ export class PackageComponent {
     //
     //     });
   }
+
+  dragStart(e, id, type) {
+    console.log('drag start: ' + e);
+    // console.log(id);
+    e.dataTransfer.setData('id', id);
+    e.dataTransfer.setData('type', type);
+  }
+
+  onDrop(e) {
+    console.log('drop: ' + e);
+    console.log(e.dataTransfer.getData('id'));
+    console.log(e.dataTransfer.getData('type'));
+  }
+
+  onRelease(e) {
+    //reset
+    this.package.processes = this.package.processes.filter((item) => {
+      return item['type'] != 'placeholder'
+    });
+  }
+
+  allowDrop(e, index: number) {
+
+    interface Placeholder {
+        type: string;
+        order: number;
+    }
+    // console.log(index);
+    var element = e.target as HTMLElement;
+    // console.log(e.pageY);
+    // console.log(e.pageY - element.getBoundingClientRect().top);
+    if (Math.abs(index % 1) > 0.4 && Math.abs(index % 1) < 0.6) {
+      return
+    }
+    if (e.pageY - element.getBoundingClientRect().top > element.offsetHeight * 0.5) {
+      // drop below elements
+      // console.log('s');
+
+      var ind = index + 0.5;
+      if (ind != this.placeholderLocation) {
+        console.log(ind);
+        // console.log(index);
+        console.log(this.placeholderLocation);
+        this.placeholderLocation = ind;
+        this.package.processes = this.package.processes.filter((item) => {
+          return item.type != 'placeholder'
+        });
+        let temp = <Placeholder> {type: "placeholder", order: ind};
+        this.package.processes.splice(index + 1, 0, temp);
+      }
+
+    } else {
+      //drop above element.
+      // this.placeholderLocation = index + 0.5;
+      var ind = index - 0.5;
+      if (ind != this.placeholderLocation) {
+        console.log(ind);
+        // console.log(index);
+        console.log(this.placeholderLocation);
+        this.placeholderLocation = ind;
+        this.package.processes = this.package.processes.filter((item) => {
+          return item.type != 'placeholder'
+        });
+        let temp = <Placeholder> {type: "placeholder", order: ind};
+        this.package.processes.splice(index, 0, temp);
+      }
+    }
+    // console.log(element.offsetHeight);
+
+
+    e.preventDefault();
+  }
+
+  //POST to create a new process from package_id and module_id
+  // post module, package and order
+  // let data = {"module":module.module_id, "package":package_id}
+  // if (this.processes.length > 0) {
+  //     data['order'] = this.processes[this.processes.length-1].order+1;
+  // } else {
+  //     data['order'] = 0;
+  // }
+  // this.client.fetch('/api/process/', {
+  //     method: "POST",
+  //     body: json(data),
+  // })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //         this.processes = data;
+  //     });
 }
--- Angular6/src/app/app.module.ts
+++ Angular6/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {NgxChartsModule} from '@swimlane/ngx-charts';
+// import {NgxDnDModule} from '@swimlane/ngx-dnd';
+// import { DragulaModule } from 'ng2-dragula';
+// import { DragDropDirectiveModule} from "angular4-drag-drop";
+// import { DndListModule } from 'ngx-drag-and-drop-lists';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +26,10 @@ import { NotFoundComponent } from './NotFound.component';
     BrowserModule,
     HttpClientModule,
     NgxChartsModule,
+    // DndListModule,
+    // NgxDnDModule,
+    // DragulaModule,
+    // DragDropDirectiveModule,
     PackagesModule,
     AppRoutingModule,
     BrowserAnimationsModule
